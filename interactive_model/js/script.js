6112c8aba502d40ffa84ef62e3450b51157847df
thisweidesign <thisweidesign@gmail.com>
2019-03-12 20:44:52 -0700

    Update script.js

--- interactive_model/js/script.js
+++ interactive_model/js/script.js
@@ -37,7 +37,7 @@ controls.enableZoom = true;
 
 function init() {
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.lookAt(scene.position);
 	camera.position.z = 500;
 
